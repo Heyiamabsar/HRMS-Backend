c32d2260eb54c6377a910b1ac2d8c67e96fbc61c
faisal <chaushfaisal5@gmail.com>
2025-05-17 14:18:55 +0530

    upload

---
+++ controllers/fileUploadController.js
@@ -0,0 +1,100 @@
+import UploadModel from "../models/fileUploadModel.js";
+import mongoose from 'mongoose';
+import UserModel from "../models/userModel.js";
+
+
+export const handleFileUpload = async (req, res) => {
+  try {
+
+    if (!req.files || req.files.length === 0) {
+      return res.status(400).json({ error: 'No files uploaded' });
+    }
+
+    const files = req.files.map((file) => ({
+        _id: new mongoose.Types.ObjectId(),
+      filename: file.originalname,
+      url: file.path,
+      mimetype: file.mimetype,
+      size: file.size,
+    }));
+    
+
+    const newUpload = new UploadModel({
+      title: req.body.title || 'Untitled',
+      files,
+      user: req.body.userId ? req.body.userId : null,
+    });
+
+    const savedUpload = await newUpload.save();
+
+    await UserModel.findByIdAndUpdate(req.body.userId, {
+      $push: { uploads: savedUpload },
+    });
+
+    const populatedUpload = await UploadModel.findById(savedUpload._id).populate('user');
+
+    res.status(200).json({
+      message: `${req.files.length} Files uploaded successfully`,
+      count: req.files.length,
+      title: req.body.title,
+      files,
+      user: populatedUpload.user
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+};
+
+
+
+export const getAllUploads = async (req, res) => {
+  try {
+    const uploads = await UploadModel.find().populate('user');
+    res.status(200).json(uploads);
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+};
+
+
+export const getUploadById = async (req, res) => {
+  const { id } = req.params;
+console.log("Get Upload ID:", id);
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: 'Invalid ID format' });
+  }
+
+  try {
+    const upload = await UploadModel.findById(id).populate('userId');
+    if (!upload) {
+      return res.status(404).json({ error: 'Upload not found' });
+    }
+
+    res.status(200).json({
+      ...upload.toObject(),
+      user: upload.userId
+    });
+  } catch (error) {
+    console.error("Get Upload Error:", error);
+    res.status(500).json({ error: 'Server error' });
+  }
+};
+export const deleteUpload = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: 'Invalid ID format' });
+  }
+
+  try {
+    const upload = await UploadModel.findByIdAndDelete(id);
+    if (!upload) {
+      return res.status(404).json({ error: 'Upload not found' });
+    }
+
+    res.status(200).json({ message: 'Upload deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+};
