39e8b75053280e82226d404d665aaf4d538c8689
faisal <chaushfaisal5@gmail.com>
2025-05-19 14:23:14 +0530

    added upload APis

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -2,78 +2,15 @@
 import express from 'express';
 import multer from 'multer';
 import { storage } from '../config/cloudinary.config.js'; 
-import { getAllUploads, getUploadById, handleFileUpload } from '../controllers/fileUploadController.js';
+import { deleteUpload, getAllUploads, getUploadById, handleFileUpload } from '../controllers/uploadController.js';
+
 
 const uploadRouter = express.Router();
 const upload = multer({ storage });
 
 uploadRouter.post('/upload', upload.array('files', 10), handleFileUpload);
-
 uploadRouter.get('/uploads', getAllUploads);
 uploadRouter.get('/uploads/:id', getUploadById);
+uploadRouter.delete('/uploads/:id', deleteUpload);
 
 export default uploadRouter;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import connectDB from "./config/dbConnection.js";
 import cors from "cors";
 import employeeRouter from "./routes/employeeRoutes.js";
 import hrRouter from "./routes/hrRoutes.js";
+import uploadRouter from "./routes/uploadRoutes.js";
 
 
 dotenv.config();
@@ -21,6 +22,7 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/employee", employeeRouter);
 app.use("/api/hr", hrRouter);
+app.use('/api', uploadRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
