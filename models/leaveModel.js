d3a803545857325f636b882fc308a0e68f68bf75
faisal <chaushfaisal5@gmail.com>
2025-05-20 09:55:44 +0530

    pull from faisal to main

---
+++ models/leaveModel.js
@@ -0,0 +1,25 @@
+
+import mongoose from 'mongoose';
+
+const leaveSchema = new mongoose.Schema({
+  employee: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+
+  },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  reason: { type: String, required: true },
+  maximumLeave: { type: Number, required: true ,default: 20 },
+  leaveTaken: { type: Number, required: true, default: 0 },
+  leaveBalance: { type: Number, required: true  , default: 20 },
+  leaveType: { type: String, enum: ['vacation', 'sick', 'casual', 'paid', 'LOP'], required: true },
+  fromDate: { type: Date, required: true },
+  toDate: { type: Date, required: true },
+  status: { type: String, enum: ['pending', 'approved', 'rejected'], default: 'pending' },
+
+
+  appliedAt: { type: Date, default: Date.now },
+}, { timestamps: true });
+
+export default mongoose.model('Leave', leaveSchema);
