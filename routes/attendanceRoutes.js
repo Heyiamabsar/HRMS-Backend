920a2fd4fe87d9d3045de6e4eda118e43eca4256
faisal <chaushfaisal5@gmail.com>
2025-05-22 12:23:12 +0530

    atendance path changes

--- routes/attendanceRoutes.js
+++ routes/attendanceRoutes.js
@@ -5,8 +5,8 @@ import { authenticate, authorizeRoles } from '../middleware/auth.js';
 
 const attendanceRouter = express.Router();
 
-attendanceRouter.post('/in', authenticate, authorizeRoles('admin', 'hr', 'employee'), markInTime);
-attendanceRouter.post('/out', authenticate, authorizeRoles('admin', 'hr', 'employee'), markOutTime);
+attendanceRouter.post('/check_in', authenticate, authorizeRoles('admin', 'hr', 'employee'), markInTime);
+attendanceRouter.post('/check_out', authenticate, authorizeRoles('admin', 'hr', 'employee'), markOutTime);
 
 attendanceRouter.get('/today_attendance', authenticate, authorizeRoles('admin', 'hr', 'employee'), getTodayAttendance);
 attendanceRouter.get('/single_user_attendance_history', authenticate, authorizeRoles('admin', 'hr', 'employee'), getSingleUserFullAttendanceHistory);
