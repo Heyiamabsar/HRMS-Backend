afb61e355474e2586eedd8cf5875d03f868d5ee2
faisal <chaushfaisal5@gmail.com>
2025-06-09 09:54:52 +0530

    auth responce handled

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,14 +6,22 @@ dotenv.config();
 
 export const authenticate = async(req, res, next) => {
   const token = req.header('Authorization')?.replace('Bearer ', '');
-  if (!token) return res.status(401).send('Access Denied');
+  if (!token) return res.status(401).send({
+    success: false,
+    statusCode: 401,
+    message: 'Access denied. No token provided.'
+  });
 
   try {
     const verified = jwt.verify(token, process.env.JWT_SECRET);
     req.user = verified;
     next();
   } catch (err) {
-    res.status(400).send('Invalid Token');
+    res.status(400).send({
+      success: false,
+      statusCode: 400,
+      message: 'Invalid Token'
+    });
   }
 };
 
@@ -22,14 +30,27 @@ export const authorizeRoles = (...roles) => {
     try {
       
       const user = await userModel.findById(req.user._id).select('role');
-      if (!user) return res.status(404).json({ message: 'User not found' });
+      if (!user) return res.status(404).json({
+        success: false,
+        statusCode: 404,
+        message: 'User not found'
+      });
 
       if (!roles.includes(user.role)) {
-        return res.status(403).json({ message: 'Access denied' });
+        return res.status(403).json({
+          success: false,
+          statusCode: 403,
+          message: 'Access denied'
+        });
       }
       next();
     } catch (err) {
-      res.status(500).json({ message: 'Server Error', error: err.message });
+      res.status(500).json({
+        success: false,
+        statusCode: 500,
+        message: 'Server Error',
+        error: err.message
+      });
     }
   };
 };
@@ -41,7 +62,11 @@ export const isVerifiedPass = async (req, res, next) => {
         const user = await userModel.findOne({ email })
 
    if (!user || !(await bcrypt.compare(password, user.password))) {
-      return res.status(401).json({ message: 'Invalid credentials' });
+      return res.status(401).json({
+        success: false,
+        statusCode: 401,
+        message: 'Invalid credentials'
+      });
     }
 
 
