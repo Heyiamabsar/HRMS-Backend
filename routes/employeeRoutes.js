42f7264c85e9cdb845b47800988620ef2e17cfbf
faisal <chaushfaisal5@gmail.com>
2025-08-13 17:24:57 +0530

    refactor: reorganize employeeRouter routes for improved readability

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -4,22 +4,24 @@ import { authenticate, authorizeRoles } from '../middleware/auth.js';
 const employeeRouter = express.Router();
 
 // employeeRouter.get('/dashboard', authenticate, getDashboard);
-
 employeeRouter.use(authenticate);
-employeeRouter.get('/', authorizeRoles('admin', 'hr'), getAllUsers);
+employeeRouter.patch('/update_profile_by_self', authorizeRoles('admin', 'hr','employee'), updateProfileBySelf);
+
+
 employeeRouter.get('/deleted', authorizeRoles('admin', 'hr'), getAllDeletedUsers);
+employeeRouter.get('/', authorizeRoles('admin', 'hr'), getAllUsers);
 employeeRouter.get('/designation', getAllDesignations);
 employeeRouter.get('/department', getAllDepartments);
+
 employeeRouter.get('/:id', authorizeRoles('admin', 'hr', 'employee'), getUserById);
 employeeRouter.put('/:id', authorizeRoles('admin', 'hr'), updateUser);
 employeeRouter.patch('/:id', authorizeRoles('admin', 'hr'), deleteUser);
-employeeRouter.patch('/update_profile_by_self', authorizeRoles('admin', 'hr','employee'), updateProfileBySelf);
 
 employeeRouter.put('/reset_password/:id', authorizeRoles('admin'), updateUserPassword);
 employeeRouter.post('/timezone', authorizeRoles('admin', 'hr', 'employee'), saveUserTimeZone);
 
 export default employeeRouter;
 
-
+ 
 
 
